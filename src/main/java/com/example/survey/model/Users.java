df5277b6dbff3ac7e13b5023e2ab2d44847f4aab
Kuba <kulajakub196@gmail.com>
2023-08-20 20:28:52 +0200

    Add user

---
+++ src/main/java/com/example/survey/model/Users.java
@@ -0,0 +1,20 @@
+package com.example.survey.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Users {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column
+    private String username;
+    @Column
+    private String passowrd;
+
+    public Users() {
+    }
+}
