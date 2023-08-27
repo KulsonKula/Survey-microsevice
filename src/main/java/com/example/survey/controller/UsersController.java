68d1d5640a9adaca36b31a51ac52863aba33931a
Kuba <kulajakub196@gmail.com>
2023-08-27 19:45:20 +0200

    Start dokumentacji

--- src/main/java/com/example/survey/controller/UsersController.java
+++ src/main/java/com/example/survey/controller/UsersController.java
@@ -2,11 +2,14 @@ package com.example.survey.controller;
 
 import com.example.survey.model.Users;
 import com.example.survey.repository.UsersRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User")
 @RestController
 @RequestMapping("/api/user")
 public class UsersController {
@@ -17,12 +20,18 @@ public class UsersController {
     }
 
     @DeleteMapping("/delete/{user_id}")
+    @Operation(
+            description = "Delete a specific User"
+    )
     public ResponseEntity<HttpStatus> deleteUserById(@PathVariable Integer user_id) {
         usersRepository.deleteById(Long.valueOf(user_id));
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("/add")
+    @Operation(
+            description = "Add user to database"
+    )
     public ResponseEntity<Users> createUser(@RequestBody Users users) {
         users.setId(null);
         usersRepository.save(users);
@@ -30,6 +39,9 @@ public class UsersController {
     }
 
     @GetMapping("/login")
+    @Operation(
+            description = "Check if username and password are in the database"
+    )
     public ResponseEntity<Integer> getUser(@ParameterObject Users users) {
         Users newUsers = usersRepository.findByUsernameAndPassword(users.getUsername(), users.getPassword());
         if (newUsers == null) {
@@ -39,6 +51,9 @@ public class UsersController {
     }
 
     @PostMapping("/edit/{user_id}")
+    @Operation(
+            description = "Update user"
+    )
     public ResponseEntity<HttpStatus> updateUser(@RequestBody Users users, @PathVariable int user_id) {
         users.setId(user_id);
         usersRepository.save(users);
